0902e6eba6ee175e249c4d050194ae882d316fdb
cabbage16 <qocn16@gmail.com>
2026-08-27 14:48:20 +0900

    refactor: 인증 코드의 유효 시간을 설정으로 분리
    Repository와 Sender에 각각 하드코딩 되어 있던 3분이라는 유효시간을 VerificationCodeProperties로 통합했습니다.

--- src/main/kotlin/com/duro/kukie/user/infrastructure/SmtpVerificationCodeSender.kt
+++ src/main/kotlin/com/duro/kukie/user/infrastructure/SmtpVerificationCodeSender.kt
@@ -1,5 +1,6 @@
 package com.duro.kukie.user.infrastructure
 
+import com.duro.kukie.global.config.properties.VerificationCodeProperties
 import com.duro.kukie.global.mail.MailClient
 import com.duro.kukie.user.application.port.VerificationCodeSender
 import org.springframework.stereotype.Component
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component
 @Component
 class SmtpVerificationCodeSender(
     private val mailClient: MailClient,
+    private val verificationCodeProperties: VerificationCodeProperties,
 ) : VerificationCodeSender {
 
     override fun send(email: String, code: String) {
@@ -17,7 +19,7 @@ class SmtpVerificationCodeSender(
                 <h1>이메일 인증 번호</h1>
                 <p>아래 인증 번호를 입력하여 가입을 완료해 주세요.</p>
                 <h2>$code</h2>
-                <p>※ 인증 번호는 3분 동안만 유효합니다.</p>
+                <p>※ 인증 번호는 ${verificationCodeProperties.expiration.toMinutes()}분 동안만 유효합니다.</p>
             """.trimIndent(),
         )
     }
